9706e0e323e69dd9c0e6ab67b54b257ad48aa09d
naphaly <hoangp051@gmail.com>
2025-07-24 11:35:02 +0700

    Second commit release, update files location

--- scripts/copy-eth-ip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ip -4 addr show $(ip route | awk '/default/ {print $5}') | \
-grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n1 | xclip -selection clipboard
-
-notify-send "Ethernet IP copied" "$(xclip -o -selection clipboard)"
--- scripts/media/scroll_spotify.sh
+++ scripts/media/scroll_spotify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Infinite loop to stream the latest scroll output for Polybar
-~/.config/eww/scripts/module/scroll_media -t 4 -l 20 | while read -r line; do
+~/.config/eww/scripts/media/scroll_media -t 4 -l 20 | while read -r line; do
     echo "$line"
     sleep 0.4
 done
